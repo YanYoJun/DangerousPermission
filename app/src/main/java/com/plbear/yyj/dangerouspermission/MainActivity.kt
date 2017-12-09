10e5b31ceef21475be0ca4135959835b88bd9669
yanyongjun <yanyojun@foxmail.com>
2017-12-09 22:29:40 +0800

    Initial commit

---
+++ app/src/main/java/com/plbear/yyj/dangerouspermission/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.plbear.yyj.dangerouspermission
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.AppCompatTextView
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClick_requestPermission(v: View) {
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0);
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        var i = 0;
+        while (i < permissions.size) {
+            Log.e(TAG, "permission:" + permissions[i] + " grantResult:" + grantResults[i])
+            i++
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+}
