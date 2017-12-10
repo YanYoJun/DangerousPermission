adbd382cc8d6baa2491a3d1d65d8bbb9e269fb0c
yanyongjun <yanyojun@foxmail.com>
2017-12-10 09:55:35 +0800

    Glide的使用

--- app/src/main/java/com/plbear/yyj/dangerouspermission/MainActivity.kt
+++ app/src/main/java/com/plbear/yyj/dangerouspermission/MainActivity.kt
@@ -10,9 +10,11 @@ import android.support.v7.widget.AppCompatTextView
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val IMG_URI = "https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=5b4f6ad1a74bd11310c0bf603bc6cf6a/023b5bb5c9ea15cea5096eabbc003af33b87b2a9.jpg"
     val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,5 +40,14 @@ class MainActivity : AppCompatActivity() {
             i++
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun onclick_loadImageView(v: View) {
+        Glide.with(this)
+                .load(IMG_URI)
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.error)
+                .into(img)
+
     }
 }
